43c0a2f879a9c9720305bb77e45ce8b9eb1817d1
hhtx <1420185659@qq.com>
2020-02-01 11:48:01 +0800

    修复krgen init无法执行的bug

--- bin/krgen-init.js
+++ bin/krgen-init.js
@@ -1,3 +1,5 @@
 #!/usr/bin/env node
 
-require('rget-cli')('https://github.com:rgy19930329/ky-react-template#master');
+require('rget-cli')('https://github.com:rgy19930329/ky-react-template#master', {
+    replaceFiles: ['package.json', 'README.md', 'template.html']
+});
